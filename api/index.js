98c61ca9a90bd92246788ff9deaa48580cc16e76
Aditya Pandey <adityapandey272002@gmail.com>
2023-11-23 23:54:37 +0530

    multer for image upload and all the routes are created

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express=require('express')
 const app = express()
 const dotenv=require('dotenv')
 const mongoose=require('mongoose')
-
+const multer = require("multer");
+const path = require("path");
 
 const authRoute=require('./routes/auth')
 const userRoute=require('./routes/users')
 const postRoute=require('./routes/post')
+const categoryRoute=require('./routes/categories')
 
 
 dotenv.config();
@@ -22,9 +24,25 @@ mongoose.connect(process.env.MONGO_URL).then(
 
 
 
+// storage for image
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
+
 app.use('/api/auth',authRoute);
 app.use('/api/users',userRoute);
 app.use('/api/posts',postRoute);
+app.use('/api/categories',categoryRoute);
 
 
 
